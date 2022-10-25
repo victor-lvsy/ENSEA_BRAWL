29e01d5dbcee30a4374bea9469e77cc64c59c209
Victor <victorlevesy@gmail.com>
2022-10-25 14:54:02 +0200

    V1

--- src/Initialisation.java
+++ src/Initialisation.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+//
 public class Initialisation {
     public ArrayList<String> getNameList() {
         return nameList;
